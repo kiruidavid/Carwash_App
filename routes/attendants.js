ef92086ffeee6003f000998779208d97061bf16f
kiruidavid <david.wendot@gmail.com>
2022-11-16 12:37:48 +0300

    removed repeated files

--- routes/attendants.js
+++
@@ -1,9 +0,0 @@
-import {Router} from 'express' 
-import {addAttendant, getAttendants} from '../controllers/attendants.js'
-
-const attendantsRouter = Router() 
-
-attendantsRouter.post('/', addAttendant) 
-attendantsRouter.get('/', getAttendants)
-
-export default attendantsRouter
